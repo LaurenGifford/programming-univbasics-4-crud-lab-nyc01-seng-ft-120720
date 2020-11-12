d64fa0e7d7f427532d837b573f1e51c9d662922f
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-12 23:29:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = []
-  array.pop([-1])
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
   array = []
-  array.shift([1])
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
