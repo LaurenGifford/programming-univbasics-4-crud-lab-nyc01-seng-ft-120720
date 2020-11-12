824239922aba11fd8b45b3105e3972ecec636e59
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-12 23:38:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Alvin", "Lauren", "Magic", "Beary"]
-  array.pop("Beary")
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
