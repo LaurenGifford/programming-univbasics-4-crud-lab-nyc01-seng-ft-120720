1e3744cd35c291d41bfb57d47db4fb7b9d507d46
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-12 23:37:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,20 +28,20 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["Alvin", "Lauren", "Magic", "Beary"]
-  array.([1])
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
   array = ["Alvin", "Lauren", "Magic", "Beary"]
-  array.([0])
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
   array = ["Alvin", "Lauren", "Magic", "Beary"]
-  array.([3])
+  array[3]
 end
 
 def update_element_from_index(array, index_number, element)
 array = ["Alvin", "Lauren", "Magic", "Beary"]
-
+array[2] = "Hibou"
 end
